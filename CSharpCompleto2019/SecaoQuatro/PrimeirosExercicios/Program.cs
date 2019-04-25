71cbe57f3c500d99e971820d97d38bb4e52c11f6
Wendell Souza <educa.wendellsouza@gmail.com>
2019-04-25 15:47:39 -0300

    *SEÇÃO 4* [Resumo de alteração] **Primeiros Exercicios com classe: Classe Pessoa** - Logica pronta e exercicio finalizado **Exercicio Triangulo** - Logica iniciada, faltando métodos na classe. [Maiores informações] - Updade do PDF dos exercicios juntamente com algumas aulas até a Seção 4. [Nota] - Acontecerá updates de novos arquivos em PDF que não serão infromados.

--- CSharpCompleto2019/SecaoQuatro/PrimeirosExercicios/Program.cs
+++ CSharpCompleto2019/SecaoQuatro/PrimeirosExercicios/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace PrimeirosExercicios
 {
@@ -10,6 +11,159 @@ namespace PrimeirosExercicios
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Estrutura de exercicios da Seção 4");
+            Console.WriteLine();
+
+            Console.WriteLine("Aperte 'Enter' para iniciar");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Digite a opção que desejar para entrar no programa correspondente:");
+            Console.WriteLine();
+
+            Console.WriteLine("Programa 'A' - Qual a maior idade");
+            Console.WriteLine("Programa 'B' - Qual a média dos salarios");
+            //Console.WriteLine("Programa 'C' - Qual a media? WHILE");
+            //Console.WriteLine("Programa 'D' - Qual a media? FOR");
+            //Console.WriteLine("Programa 'E' - Dividindo dois números");
+            //Console.WriteLine("Programa 'F' - Fatorial!");
+            //Console.WriteLine("Programa 'G' - Quais são so divisiveis?");
+            //Console.WriteLine("Programa 'H' - Imprimindo 'N' linhas");
+
+            Console.WriteLine();
+            Console.Write("Programa: ");
+            char opcao = char.Parse(Console.ReadLine());
+
+            while (opcao != 0)
+            {
+                if (opcao == 'A' || opcao == 'a')
+                {
+                    Console.Clear();
+                    Console.WriteLine("Programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais velha.");
+                    Console.WriteLine();
+                    Console.WriteLine("Pressiona qualquer tecla para iniciar o programa");
+                    Console.ReadKey();
+                    Console.Clear();
+
+                    Pessoa pessoaA, pessoaB;
+
+                    pessoaA = new Pessoa();
+
+                    Console.WriteLine("Informe o nome da primeira pessoa e o sexo, sendo (M)asculino e (F)eminino");
+                    Console.Write("Nome: ");
+                    pessoaA.Nome = Console.ReadLine();
+                    Console.Write("Sexo: ");
+                    pessoaA.Genero = char.Parse(Console.ReadLine());
+
+                    while (pessoaA.Genero != 'M' && pessoaA.Genero != 'F')
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Sexo divergente do programado.");
+                        Console.WriteLine();
+                        Console.WriteLine($"Nome: {pessoaA.Nome}");
+                        Console.WriteLine("Selecione 'F' para Feminino e 'M' para Maculino");
+                        Console.Write("Sexo: ");
+                        pessoaA.Genero = char.Parse(Console.ReadLine());
+                    }
+
+                    Console.Clear();
+                    Console.WriteLine("Informe a idade da primeira pessoa e o sexo, sendo (M)asculino e (F)eminino");
+                    Console.Write("Idade: ");
+                    pessoaA.Idade = int.Parse(Console.ReadLine());
+
+                    Console.Clear();
+
+                    pessoaB = new Pessoa();
+
+                    Console.WriteLine("Informe o nome da segunda pessoa");
+                    Console.Write("Nome: ");
+                    pessoaB.Nome = Console.ReadLine();
+                    Console.Write("Sexo: ");
+                    pessoaB.Genero = char.Parse(Console.ReadLine());
+
+                    while (pessoaB.Genero != 'M' && pessoaB.Genero != 'F')
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Sexo divergente do programado.");
+                        Console.WriteLine();
+                        Console.WriteLine($"Nome: {pessoaB.Nome}");
+                        Console.WriteLine("Selecione 'F' para Feminino e 'M' para Maculino");
+                        Console.Write("Sexo: ");
+                        pessoaB.Genero = char.Parse(Console.ReadLine());
+                    }
+
+                    Console.Clear();
+                    Console.WriteLine("Informe a idade da segunda pessoa"); ;
+                    Console.Write("Idade: ");
+                    pessoaB.Idade = int.Parse(Console.ReadLine());
+
+                    Console.Clear();
+
+                    Console.WriteLine("Na próxima tela, apresentaremos a pessoa de maior idade.");
+
+                    Console.Clear();
+
+                    if (pessoaA.Idade == pessoaB.Idade)
+                    {
+                        Console.WriteLine($"{pessoaA.Nome} tem a mesma idade que {pessoaB.Nome}. {pessoaA.Idade} anos.");
+                    }
+                    else if (pessoaA.Idade > pessoaB.Idade && pessoaA.Genero == 'M')
+                    {
+                        Console.WriteLine($"O {pessoaA.Nome} é o mais velho. Com {pessoaA.Idade} anos.");
+                    }
+                    else if (pessoaA.Idade > pessoaB.Idade && pessoaA.Genero == 'F')
+                    {
+                        Console.WriteLine($"O {pessoaA.Nome} é a mais velha. Com {pessoaA.Idade} anos.");
+                    }
+                    else if (pessoaA.Idade < pessoaB.Idade && pessoaB.Genero == 'M')
+                    {
+                        Console.WriteLine($"O {pessoaB.Nome} é o mais velho. Com {pessoaB.Idade} anos.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"O {pessoaB.Nome} é a mais velha. Com {pessoaB.Idade} anos.");
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                    Console.WriteLine("Aperte qualquer tecla para sair do programa.");
+                    Console.ReadKey();
+                }
+
+                else if (opcao == 'B' || opcao == 'b')
+                {
+                    Console.Clear();
+                    Console.WriteLine("Programa que define a média entre os salarios de dois Funcionários pessoas");
+                    Console.WriteLine();
+                    Console.WriteLine("Pressiona qualquer tecla para iniciar o programa");
+                    Console.ReadKey();
+                    Console.Clear();
+
+                    Pessoa pessoaA, pessoaB;
+
+                    pessoaA = new Pessoa();
+                    Console.WriteLine("Dados do Primeiro Funcionário");
+                    Console.Write("Nome: ");
+                    pessoaA.Nome = Console.ReadLine();
+                    Console.Write("Salario: ");
+                    pessoaA.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    Console.WriteLine();
+
+                    pessoaB = new Pessoa();
+                    Console.WriteLine("Dados do Segundo Funcionário");
+                    Console.Write("Nome: ");
+                    pessoaB.Nome = Console.ReadLine();
+                    Console.Write("Salario: ");
+                    pessoaB.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    double salario = (pessoaA.Salario + pessoaB.Salario) / 2;
+
+                    Console.Clear();
+
+                    Console.WriteLine($"O Salario médio é {salario.ToString("F2", CultureInfo.InvariantCulture)}");
+                    Console.ReadKey();
+                }
+            }
         }
     }
 }
--- CSharpCompleto2019/SecaoQuatro/ProgramaDoTriangulo/Program.cs
+++ CSharpCompleto2019/SecaoQuatro/ProgramaDoTriangulo/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace ProgramaDoTriangulo
 {
@@ -10,6 +11,63 @@ namespace ProgramaDoTriangulo
     {
         static void Main(string[] args)
         {
+            #region Orientação a objetos | Exercicio 5 - Triangulo.
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as três medidas do Triangulo X: ");
+            Console.WriteLine();
+            Console.Write("Medida do Lado A: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Medida do Lado B: ");
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Medida do Lado C: ");
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Clear();
+            Console.WriteLine("Entre com as três medidas do Triangulo Y: ");
+            Console.WriteLine();
+            Console.Write("Medida do Lado A: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Medida do Lado B: ");
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Medida do Lado C: ");
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            Console.Clear();
+
+            Console.WriteLine($"A Area do triangulo X é de {areaX.ToString("F4", CultureInfo.InvariantCulture)}cm².");
+            Console.WriteLine();
+            Console.WriteLine($"A Area do triangulo Y é de {areaY.ToString("F4", CultureInfo.InvariantCulture)}cm².");
+
+            Console.ReadKey();
+
+
+            if (areaX == areaY)
+            {
+                Console.WriteLine();
+                Console.WriteLine("As areas dos triangulos são iguais.");
+            }
+
+            else if (areaX > areaY)
+            {
+                Console.WriteLine();
+                Console.WriteLine("A Area do maior triangulo é area do triangulo X, com " + areaX.ToString("F4", CultureInfo.InvariantCulture) + "cm².");
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("A Area do maior triangulo é area do triangulo Y, com " + areaY.ToString("F4", CultureInfo.InvariantCulture) + "cm².");
+            }
+
+            #endregion
         }
     }
 }
