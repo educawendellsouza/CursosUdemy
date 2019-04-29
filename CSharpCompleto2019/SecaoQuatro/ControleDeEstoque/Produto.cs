4759e1eb0f1bff3abde9dff97a8e14e670946b99
Wendell Souza <educa.wendellsouza@gmail.com>
2019-04-29 09:56:33 -0300

    *SEÇÃO 04* [Alteração no código] - Início do programa de Controle de Estoque

---
+++ CSharpCompleto2019/SecaoQuatro/ControleDeEstoque/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeEstoque
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int QuantidadeEmEstoque;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * QuantidadeEmEstoque;
+        }
+    }
+}
---
+++ CSharpCompleto2019/SecaoQuatro/ControleDeEstoque/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeEstoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.WriteLine();
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Preço: ");
+            p.Preco = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Quantidade em Estoque: ");
+            p.QuantidadeEmEstoque = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Dados do produto: ");
+        }
+    }
+}
