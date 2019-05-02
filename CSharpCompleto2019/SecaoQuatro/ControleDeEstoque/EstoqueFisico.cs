4766848acc2169858fece35f4ab1568a27daf433
Wendell Souza <educa.wendellsouza@gmail.com>
2019-05-02 11:08:11 -0300

    *SEÇÃO 04* [Resumo das Alterações] - Inserção da casse ControleDeEstoque

---
+++ CSharpCompleto2019/SecaoQuatro/ControleDeEstoque/EstoqueFisico.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+
+namespace ControleDeEstoque
+{
+    class EstoqueFisico
+    {
+        public List<Produto> Estoque { get; set; }
+        private string caminhoJSON = "c:\\Users\\Wendell S\\source\repos\\educawendellsouza\\CursosUdemy\\CSharpCompleto2019\\SecaoQuatro\\ControleDeEstoque";
+
+        public void Salvar()
+        {            
+            System.IO.File.WriteAllText(caminhoJSON, new JavaScriptSerializer().Serialize(Estoque));
+        }
+
+        public void Carregar()
+        {
+            Estoque = new JavaScriptSerializer().Deserialize<List<Produto>>(System.IO.File.ReadAllText(caminhoJSON));
+        }
+    }
+}
