8bc1b2a148bdca90884a59ac1d13fd064b0b0714
Wendell Souza <educa.wendellsouza@gmail.com>
2019-06-06 15:40:42 -0300

    *SEÇÃO 04*
    [Resumo das Alterações]
    [Exercicio2]
    - Inserção da Classe Funcionario
    - Alteração inicial de Program

---
+++ CSharpCompleto2019/SecaoQuatro/Exercício2/Funcionario.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercício2
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+        public double Aumento;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto -= Imposto;
+        }
+
+        public void AumentarSalarioEmPorcentagem(double porcentagem)
+        {
+            double i = SalarioBruto / 100 * porcentagem;
+            SalarioBruto += i;
+        }
+
+        public double NovoSalario (double porcentos)
+        {
+            double i = SalarioBruto / 100;
+            double diferenca = SalarioBruto - porcentos;
+            if(diferenca < 0)
+            {
+                diferenca *= -1;
+                return Aumento = diferenca / i;
+            }
+            else
+            {
+                return Aumento = diferenca / i;
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Nome do Funcionário: {0} \r\n" +
+                                 "Salario bruto: {1} \r\n" +
+                                 "Imposto: {2}",
+                                  Nome,
+                                  SalarioBruto.ToString("F2", CultureInfo.InvariantCulture),
+                                  Imposto.ToString("F2", CultureInfo.InvariantCulture),
+                                  Aumento.ToString("F2", CultureInfo.InvariantCulture));            
+        }
+    }
+}
---
+++ CSharpCompleto2019/SecaoQuatro/Exercício2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercício2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Funcionario();
+
+            Console.Write("Digite o nome do Funcionário(a): ");
+            f.Nome = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.Write("Digite o seu salário bruto: R$ ");
+            f.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); //Digitando com ponto ou virgula para dividir as casas decimais
+
+            Console.WriteLine(); ;
+
+            Console.Write("Digite o valor do imposto que é cobrado: R$ ");
+            f.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture); //Digitando com ponto ou virgula para dividir as casas decimais
+
+            f.SalarioLiquido();
+            Console.Clear();
+
+            Console.WriteLine(f);
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Escolha a opção que você deseja: ");
+            Console.WriteLine("Opção A: Previsão de quanto o funcionário deverá ter de aumento");
+            Console.WriteLine("Opção B: Aumentar o salário do Funcionário");
+            Console.WriteLine("Opção C: Mostrar na tela a atual situação do Funcionário(a)");
+
+            Console.Write("Digite quanto que o funcionário deverá ganhar: R$ ");
+            f.NovoSalario(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine(f.Aumento + " %");
+
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.Write("Digite a porcentagem para aumentar o salario: ");
+            f.AumentarSalarioEmPorcentagem(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.Clear();
+
+            Console.WriteLine(f);
+            Console.ReadKey();
+
+        }
+    }
+}
