dccb905168c94255061615bb7d2d71b960cff07e
Wendell Souza - PC Lenovo <educa.wendellsouza@gmail.com>
2019-06-27 18:34:55 -0300

    *SEÇÃO 04* [Programa Funcionário] - Inserção de lógica para resolução do exercicio final da Seção 04

---
+++ CSharpCompleto2019/SecaoQuatro/Funcionario/FuncionarioPessoa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Funcionario
+{
+    class FuncionarioPessoa
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto += porcentagem / 100 * SalarioBruto;
+        }
+    }
+}
---
+++ CSharpCompleto2019/SecaoQuatro/Funcionario/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Funcionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FuncionarioPessoa f = new FuncionarioPessoa();
+
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+            Console.WriteLine("");
+            Console.Write("Salário bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("");
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Clear();
+
+            Console.WriteLine("Funcionário: {0}, R$ {1}", f.Nome, f.SalarioLiquido().ToString(("F2"), CultureInfo.InvariantCulture));
+
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            f.AumentarSalario(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Dados Atualizados: {0}, R$ {1}", f.Nome, f.SalarioBruto.ToString(("F2"), CultureInfo.InvariantCulture));
+        }
+    }
+}
