30e764772f4c6373848862f04619092ae9fe4045
Wendell Souza - PC Lenovo <educa.wendellsouza@gmail.com>
2019-07-05 10:45:44 -0300

    *SEÇÃO 05* [Projeto de Construtores e Sobrecarga] - Inicio de prejeto

---
+++ CSharpCompleto2019/SecaoCinco/ContaBancaria/ContaController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContaBancaria
+{
+    class ContaController
+    {
+        #region • Atributos Privados
+
+
+
+        #endregion
+
+        #region • Propriedades autoimplementadas
+        
+        
+
+        #endregion
+
+        #region • Construtores
+
+
+
+        #endregion
+
+        #region • Propriedades customizadas
+
+
+
+        #endregion
+
+        #region • Outros métodos da classe
+
+
+
+        #endregion
+
+    }
+}
